6ca0dac0d4cddcdf2d79671baf8f4c61fc7b1518
Troy McCabe <troymccabe@gmail.com>
2018-02-21 22:59:07 -0600

    Adds telegram, adds `addr(<addr>)` and `tx(<tx>)`

--- index.js
+++ index.js
@@ -8,19 +8,23 @@
 
 const SOURCE_DISCORD = 'discord';
 const SOURCE_SLACK = 'slack';
+const SOURCE_TELEGRAM = 'telegram';
 
 function processMessage(message, source) {
     if (!message) {
-        return;
+        return new Promise((resolve, reject) => {});
     }
     
     if (message.match(/wh?en (moon|lambo)/igm)) {
         switch (source) {
             case SOURCE_DISCORD:
-                return 'How should I know?? Go ask in <#409184678730268672>';
+                return 'How should I know? Go ask in <#409184678730268672>';
 
             case SOURCE_SLACK:
                 return 'Check with #price-prediction on Discord: https://discord.gg/X2XVaQz';
+
+            case SOURCE_TELEGRAM:
+                return 'After hard work!';
         }
     }
 
@@ -31,7 +35,9 @@ function processMessage(message, source) {
             '`?wiki.backup`: Get the link to the backup process wiki article\r\n' + 
             '`?wiki.faq`: Get the link to the FAQ wiki article\r\n' + 
             '`?wiki.installation`: Get the link to the installation wiki article\r\n' + 
-            '`?wiki.staking`: Get the link to the staking wiki article'
+            '`?wiki.staking`: Get the link to the staking wiki article\r\n' +
+            '`addr(ECC_ADDRESS)`: Shows the date first seen on the network, as well as the current balance\r\n' +
+            '`tx(TXID)`: Shows detail for a specific transaction'
         );
     }
 
@@ -55,58 +61,99 @@ function processMessage(message, source) {
         return 'https://github.com/project-ecc/ECC-Wiki/wiki/How-to-stake';
     }
 
+    var matches = message.match(/addr\((\w+)\)/i);
+    if (matches && matches.length > 1) {
+        return require('./lib/addr.js')(matches[1]);
+    }
+
+    var matches = message.match(/tx\((\w+)\)/i);
+    if (matches && matches.length > 1) {
+        return require('./lib/tx.js')(matches[1]);
+    }
+
     return;
 }
 
 /*
  * DISCORD
  */
-const Discord = require("discord.js");
+if (process.env.ECC_DISCORD_BOT_TOKEN) {
+    const Discord = require("discord.js");
 
-var discordClient = new Discord.Client();
-discordClient.on('ready', () => {});
-discordClient.on('message', message => {
-    // ignore messages from myself or other bots
-    if (message.author.bot) {
-        return;
-    }
+    var discordClient = new Discord.Client();
+    discordClient.on('ready', () => {});
+    discordClient.on('message', message => {
+        // ignore messages from myself or other bots
+        if (message.author.bot) {
+            return;
+        }
 
-    var response = processMessage(message.content, SOURCE_DISCORD);
-    if (response) {
-        message.channel.send(response).catch(reason => {});
-    }
-});
+        var response = processMessage(message.content, SOURCE_DISCORD);
+        if (response) {
+            if (typeof response == 'string') {
+                message.channel.send(response).catch(reason => {});
+            } else if (response instanceof Promise) {
+                response.then((val) => {message.channel.send(val).catch(reason => {});});
+            }
+            
+        }
+    });
 
-discordClient.login(process.env.ECC_DISCORD_BOT_TOKEN);
+    discordClient.login(process.env.ECC_DISCORD_BOT_TOKEN);
+}
 
 /*
  * SLACK
  */
-const { RtmClient, CLIENT_EVENTS, RTM_EVENTS } = require('@slack/client');
+if (process.env.ECC_SLACK_BOT_TOKEN) {
+    const { RtmClient, CLIENT_EVENTS, RTM_EVENTS } = require('@slack/client');
 
-const appData = {};
-const slackClient = new RtmClient(
-    process.env.ECC_SLACK_BOT_TOKEN, 
-    {dataStore: false, useRtmConnect: true,}
-);
+    const appData = {};
+    const slackClient = new RtmClient(
+        process.env.ECC_SLACK_BOT_TOKEN, 
+        {dataStore: false, useRtmConnect: true,}
+    );
 
-slackClient.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (connectData) => {
-  appData.selfId = connectData.self.id;
-});
+    slackClient.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (connectData) => {
+        appData.selfId = connectData.self.id;
+    });
 
-slackClient.on(RTM_EVENTS.MESSAGE, (message) => {
-    // ignore messages from myself or other bots
-    if (
-        (message.subtype && message.subtype === 'bot_message') || 
-        (!message.subtype && message.user === appData.selfId)
-    ) {
-        return;
-    }
+    slackClient.on(RTM_EVENTS.MESSAGE, (message) => {
+        // ignore messages from myself or other bots
+        if (
+            (message.subtype && message.subtype === 'bot_message') || 
+            (!message.subtype && message.user === appData.selfId)
+        ) {
+            return;
+        }
 
-    var response = processMessage(message.text, SOURCE_DISCORD);
-    if (response) {
-        slackClient.sendMessage(response, message.channel);
-    }
-});
+        var response = processMessage(message.text, SOURCE_DISCORD);
+        if (response) {
+            if (typeof response == 'string') {
+                slackClient.sendMessage(response, message.channel);
+            } else if (response instanceof Promise) {
+                response.then((val) => {slackClient.sendMessage(val, message.channel);});
+            }
+        }
+    });
 
-slackClient.start();
+    slackClient.start();
+}
+
+/*
+ * TELEGRAM
+ */
+if (process.env.ECC_TELEGRAM_BOT_TOKEN) {
+    const TelegramBot = require('node-telegram-bot-api');
+    const telegramBot = new TelegramBot(process.env.ECC_TELEGRAM_BOT_TOKEN, {polling: {interval: 0, params: {timeout: 200}}});
+
+    telegramBot.on('message', (message) => {
+        var response = processMessage(message.text, SOURCE_TELEGRAM);
+
+        if (typeof response == 'string') {
+            telegramBot.sendMessage(message.chat.id, response, {parse_mode: 'Markdown'});
+        } else if (response instanceof Promise) {
+            response.then((val) => {telegramBot.sendMessage(message.chat.id, val, {parse_mode: 'Markdown'});});
+        }
+    });
+}
